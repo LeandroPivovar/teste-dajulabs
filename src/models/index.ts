625ce6cdd1fd65f2101c2d11f5e61939d9cf4f38
LeandroPivovar <leandropivovar@hotmail.com>
2025-03-03 07:46:20 -0300

    feat: initial configs and database

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from 'sequelize';
+import sequelize from '../config/database';
+
+const db: { [key: string]: any } = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+export default db;
